e9f5dfde4198748e1de7b0eb408d7487eeaf4158
heniam1 <heniam2012@gmail.com>
2019-12-19 00:10:43 -0500

    Configured updated react

---
+++ confusion/build/precache-manifest.8bfb35f1eb3c146a084a6bdac0c499bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cdea19377d8129a6ae0f153cab242b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "311b43513336fdd2bbe3",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "8f641d6be6333ad6d900",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "311b43513336fdd2bbe3",
+    "url": "/static/js/2.a64dff8f.chunk.js"
+  },
+  {
+    "revision": "8f641d6be6333ad6d900",
+    "url": "/static/js/main.0f20ea5a.chunk.js"
+  },
+  {
+    "revision": "8870a603714713bb2741",
+    "url": "/static/js/runtime-main.17759ccd.js"
+  }
+]);
