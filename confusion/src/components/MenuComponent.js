58a8b02e4ee09f09511aab3adbbfd3df9829dfd5
heniam1 <heniam2012@gmail.com>
2020-01-15 00:14:56 -0500

    Presentational and Container Components

--- confusion/src/components/MenuComponent.js
+++ confusion/src/components/MenuComponent.js
@@ -1,21 +1,19 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay,  /*CardText, CardBody,*/CardTitle } from 'reactstrap';
-import DishDetail from './DishDetailComponent';
+
 
 
 export class Menu extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {
-          selectedDish: null
-    }
+    //     this.state = {
+    //       selectedDish: null
+    // }
 
   }
 
-  onDishSelect(dish){
-    this.setState({selectedDish: dish})
-  }
+
 
   renderDish(dish){
     // if(dish != null){
@@ -41,7 +39,7 @@ export class Menu extends Component {
             return (
               <div key={dish.id} className="col-12 col-md-5 m-1">
 
-                <Card onClick={() => this.onDishSelect(dish)}>
+                <Card onClick={()=> this.props.onClick(dish.id)}>
 
                       <CardImg width = "100%" src={dish.image} alt={dish.name} />
 
@@ -59,8 +57,6 @@ export class Menu extends Component {
             <div className="row">
                   {menu}
             </div>
-
-             <DishDetail dish={this.state.selectedDish} />
           </div>
         );
     }
