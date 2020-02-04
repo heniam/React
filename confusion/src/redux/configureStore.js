f55768318871128d3529ffd1bfd6d84d841d51ec
heniam1 <heniam2012@gmail.com>
2020-02-04 15:06:06 -0500

    React-Redux-Form revisited.
    fully functional contact form

--- confusion/src/redux/configureStore.js
+++ confusion/src/redux/configureStore.js
@@ -1,4 +1,8 @@
 import { createStore,combineReducers, applyMiddleware } from 'redux';
+
+import {createForms} from 'react-redux-form';
+import {InitialFeedback} from './forms';
+
 import { Dishes} from './dishes';
 import { Comments} from './comments';
 import { Promotions} from './promotions';
@@ -14,7 +18,10 @@ export const ConfigureStore = () => {
           dishes: Dishes,
           comments:Comments,
           promotions:Promotions,
-          leaders:Leaders
+          leaders:Leaders,
+          ...createForms({
+            feedback: InitialFeedback
+          })
         }),
 
         applyMiddleware(thunk, logger)
