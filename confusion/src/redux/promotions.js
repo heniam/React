c98946860f6d7bda0c3c20c143f91e30035cbb8f
heniam1 <heniam2012@gmail.com>
2020-02-10 04:12:24 -0500

    fetch from server
    fetching from a simple json server

--- confusion/src/redux/promotions.js
+++ confusion/src/redux/promotions.js
@@ -1,11 +1,22 @@
-import { PROMOTIONS } from '../shared/promotions';
+import * as ActionTypes from './ActionTypes';
 
+export const Promotions = (state = {
+      isLoading: true,
+      errMess: null,
+      promotions: []
+      }, action) => {
+            switch(action.type){
+              case ActionTypes.ADD_PROMOS:
+                  return {...state, isLoading: false, errMess: null, promotions: action.payload};
 
-export const Promotions = (state = PROMOTIONS, action) => {
+              case ActionTypes.PROMOS_LOADING:
+                  return {...state, isLoading: true, errMess: null, promotions: []};
 
-    switch(action.type){
-      default:
-          return state;
-    }
+              case ActionTypes.PROMOS_FAILED:
+                  return {...state, isLoading: false, errMess: action.payload, promotions:[]};
+
+              default:
+                  return state;
+            }
 
 }
