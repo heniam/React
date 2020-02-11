e538d425de6f3ab8419f4281561b56698b43d6b6
heniam1 <heniam2012@gmail.com>
2020-02-10 21:14:34 -0500

    React animation components
    Stagger , Fade and FadeTransform

--- confusion/src/components/DishDetailComponent.js
+++ confusion/src/components/DishDetailComponent.js
@@ -6,6 +6,7 @@ import {Link} from 'react-router-dom';
 import {Control, LocalForm, Errors } from 'react-redux-form';
 import {Loading} from './LoadingComponent';
 import {baseUrl} from '../shared/baseUrl';
+import {FadeTransform, Fade, Stagger} from 'react-animation-components';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -14,14 +15,19 @@ const minLength = (len) => (val) => val && (val.length >= len);
 
   function RenderDish({dish}) {
             return (
-                <div className='col-12  m-1'>
-                    <Card>
-                        <CardImg top src={baseUrl + dish.image} alt={dish.name} />
-                        <CardBody>
-                            <CardTitle>{dish.name}</CardTitle>
-                            <CardText>{dish.description}</CardText>
-                        </CardBody>
-                    </Card>
+                <div className='col-12 m-1'>
+                <FadeTransform in
+                        transformProps={{
+                              exitTransform: 'scale(0.5) translateY(-50%)'
+                        }}>
+                          <Card>
+                              <CardImg top src={baseUrl + dish.image} alt={dish.name} />
+                              <CardBody>
+                                  <CardTitle>{dish.name}</CardTitle>
+                                  <CardText>{dish.description}</CardText>
+                              </CardBody>
+                          </Card>
+                    </FadeTransform>
                 </div>
             );
         }
@@ -31,23 +37,26 @@ const minLength = (len) => (val) => val && (val.length >= len);
         if (comments != null)
         return (
           <React.Fragment>
-            <div className='col-12  m-1'>
-                <h4> Comments </h4>
+            <div className='col-12 m-1'>
+                <h4>Comments</h4>
                 <ul className='list-unstyled'>
-                    {comments.map((comment) =>{
-                      return(
-                        <li key={comment.id}>
-                        <p>{comment.comment}</p>
-                        <p>-- {comment.author} , {new Intl.DateTimeFormat('en-US', {
-                                            year: 'numeric',
-                                            month: 'short',
-                                            day: '2-digit'
-                                        }).format(new Date(comment.date))}
-                        </p>
-                        </li>
-                      );
-                    })}
-
+                    <Stagger in>
+                          {comments.map((comment) =>{
+                            return(
+                              <Fade in>
+                              <li key={comment.id}>
+                              <p>{comment.comment}</p>
+                              <p>-- {comment.author} , {new Intl.DateTimeFormat('en-US', {
+                                                  year: 'numeric',
+                                                  month: 'short',
+                                                  day: '2-digit'
+                                              }).format(new Date(comment.date))}
+                              </p>
+                              </li>
+                              </Fade>
+                            );
+                          })}
+                    </Stagger>
                 </ul>
                 <CommentForm dishId={dishId} postComment={postComment}> </CommentForm>
             </div>
